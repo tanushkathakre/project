dbdcc04b6e681aaec4d3202790cf28ccce79c4cc
tanushkathakre <104820098+tanushkathakre@users.noreply.github.com>
2024-04-22 16:43:54 +0530

    Create summ.py

---
+++ summ.py
@@ -0,0 +1,37 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from heapq import nlargest
+
+def generate_summary(text, num_sentences=3):
+    # Tokenize the text into sentences
+    sentences = sent_tokenize(text)
+    
+    # Tokenize the text into words
+    words = word_tokenize(text.lower())
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words("english"))
+    filtered_words = [word for word in words if word not in stop_words]
+    
+    # Calculate word frequency
+    word_freq = FreqDist(filtered_words)
+    
+    # Calculate sentence scores based on word frequency
+    sentence_scores = {}
+    for sentence in sentences:
+        for word in word_tokenize(sentence.lower()):
+            if word in word_freq:
+                if sentence not in sentence_scores:
+                    sentence_scores[sentence] = word_freq[word]
+                else:
+                    sentence_scores[sentence] += word_freq[word]
+    
+    # Select the top N sentences with highest scores
+    summary_sentences = nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
+    
+    # Combine the sentences into a summary
+    summary = ' '.join(summary_sentences)
+    
+    return summary
+
